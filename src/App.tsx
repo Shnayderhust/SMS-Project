2113aff1cd84c7d9652ade2145279dd6c9ed7af9
Shnayderhust <shnayderhust@gmail.com>
2024-02-22 23:19:50 +0300

    File: Some files rearangement

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import Aside from "./assets/Layerout/Aside/Aside";
 import Dashboard from "./assets/Layerout/Dashboard/Dashboard";
 import Courses from "./assets/Layerout/Courses/Courses";
 import Materials from "./assets/Layerout/Materials/Materials";
-import AnalyticsMain from "./assets/Layerout/Courses/CourseMain/Analytics/AnalyticsMain";
+// import AnalyticsMain from "./assets/Layerout/Courses/CourseMain/Analytics/AnalyticsMain";
 
 export default function App() {
   return (
     <>
       <Aside />
       <Routes>
+        <Route path="/analytics" element={<Materials />}></Route>
         <Route path="/" element={<Dashboard />}></Route>
         <Route path="/courses" element={<Courses />}></Route>
         <Route path="/materials" element={<Materials />}></Route>
-        <Route path="/analytics" element={<AnalyticsMain />}></Route>
       </Routes>
     </>
   );
